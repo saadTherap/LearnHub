fe4196fce70866788dcab8d937b06e2e8a9ed95d
Avi Dewan <avi.dewan@therapservices.net>
2025-07-23 08:56:53 +0600

    Initiated secureFileServer

---
+++ secureFileServer/src/main/java/net/therap/secureFileServer/SecureFileServerApplication.java
@@ -0,0 +1,13 @@
+package net.therap.secureFileServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureFileServerApplication.class, args);
+	}
+
+}
