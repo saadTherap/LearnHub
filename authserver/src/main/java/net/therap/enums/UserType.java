24a6e4942814b4039c6f2d0b064d7d38416b028a
Apurbo Banik Turjo <apurbo.turjo@therapservices.net>
2025-07-27 09:16:14 +0600

    Project structure initialization: Entities, controller, dto, enums, exceptions, repos, security, service, app

--- authserver/src/main/java/net/therap/enums/UserType.java
+++ authserver/src/main/java/net/therap/enums/UserType.java
@@ -1,7 +1,14 @@
 package net.therap.enums;
 
 /**
-* @author apurboturjo
-* @since 7/24/25
-*/public enum UserType {
+ * @author apurboturjo
+ * @since 7/24/25
+ */
+public enum UserType {
+    
+    STUDENT,
+    
+    INSTRUCTOR,
+    
+    ADMIN
 }
