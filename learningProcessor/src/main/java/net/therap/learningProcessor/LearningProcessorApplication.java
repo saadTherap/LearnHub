650513e70cf39cd314daf89f67baa532821619e4
Avi Dewan <avi.dewan@therapservices.net>
2025-07-28 14:35:52 +0600

    Learning-processor initialzied with gradle

---
+++ learningProcessor/src/main/java/net/therap/learningProcessor/LearningProcessorApplication.java
@@ -0,0 +1,13 @@
+package net.therap.learningProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningProcessorApplication.class, args);
+    }
+
+}
---
+++ learningProcessor/src/test/java/net/therap/learningProcessor/LearningProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.therap.learningProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
