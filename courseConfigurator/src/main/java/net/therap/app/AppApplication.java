dee293a29cb4291093434b839fc3f06ccaa58375
Riad Ahmed Anonto <riadanonto@riad.anonto-laptop>
2025-07-31 10:54:47 +0600

    fixed server dependency bug

--- courseConfigurator/src/main/java/net/therap/app/AppApplication.java
+++ courseConfigurator/src/main/java/net/therap/app/AppApplication.java
@@ -1,12 +1,18 @@
 package net.therap.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.hazelcast.HazelcastHealthContributorAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
-@EnableCaching
+@SpringBootApplication(exclude = {
+		CacheAutoConfiguration.class,
+		HazelcastAutoConfiguration.class,
+		HazelcastHealthContributorAutoConfiguration.class
+})
 public class AppApplication {
 
 	public static void main(String[] args) {
--- courseConfigurator/src/main/java/net/therap/app/config/HazelcastConfig.java
+++ courseConfigurator/src/main/java/net/therap/app/config/HazelcastConfig.java
@@ -2,9 +2,13 @@ package net.therap.app.config;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.YamlClientConfigBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+
+import java.io.InputStream;
 
 /**
  * @author riadanonto
@@ -14,11 +18,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 public class HazelcastConfig {
 
     @Bean
-    @ConditionalOnMissingBean
     public HazelcastInstance hazelcastInstance() {
-        try {
+        try (InputStream configStream = getClass().getClassLoader().getResourceAsStream("hazelcast-client.yaml")) {
+            ClientConfig clientConfig = new YamlClientConfigBuilder(configStream).build();
 
-            return HazelcastClient.newHazelcastClient();
+            return HazelcastClient.newHazelcastClient(clientConfig);
         } catch (Exception e) {
 
             System.out.println("Hazelcast not available, running without cache!");
@@ -26,4 +30,5 @@ public class HazelcastConfig {
         }
     }
 }
+
 
