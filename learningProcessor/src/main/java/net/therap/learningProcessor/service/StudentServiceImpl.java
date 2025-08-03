94941a11d4cbeca6a45c7bfd5316d3d6e369d549
Riad Ahmed Anonto <riadanonto@riad.anonto-laptop>
2025-08-03 15:48:04 +0600

    confugured hazelcast to student

--- learningProcessor/src/main/java/net/therap/learningProcessor/service/StudentServiceImpl.java
+++ learningProcessor/src/main/java/net/therap/learningProcessor/service/StudentServiceImpl.java
@@ -1,11 +1,16 @@
 package net.therap.learningProcessor.service;
 
 import lombok.RequiredArgsConstructor;
+import net.therap.learningProcessor.constants.CacheConstants;
 import net.therap.learningProcessor.dto.StudentDto;
 import net.therap.learningProcessor.entity.Student;
 import net.therap.learningProcessor.mapper.StudentMapper;
 import net.therap.learningProcessor.repository.StudentRepository;
+import net.therap.learningProcessor.util.CacheInvalidationUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.util.List;
 import java.util.Objects;
@@ -22,6 +27,7 @@ public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
     private final StudentMapper studentMapper;
+    private final CacheInvalidationUtil cacheInvalidationUtil;
 
     @Override
     public List<StudentDto> getAllStudents() {
@@ -38,14 +44,17 @@ public class StudentServiceImpl implements StudentService {
                 .orElse(null);
     }
 
+    @Transactional
     @Override
     public StudentDto createStudent(StudentDto dto) {
         Student entity = studentMapper.toEntity(dto);
         Student savedEntity = studentRepository.save(entity);
+        cacheInvalidationUtil.invalidateCachesAfterCommit(savedEntity.getId(), CacheConstants.STUDENTS);
 
         return studentMapper.toDto(savedEntity);
     }
 
+    @Transactional
     @Override
     public StudentDto updateStudent(StudentDto studentDto) {
         Student existingStudent = studentRepository.findById(studentDto.getId()).orElse(null);
@@ -56,11 +65,12 @@ public class StudentServiceImpl implements StudentService {
 
         studentMapper.updateStudentFromDto(studentDto, existingStudent);
         Student updatedStudent= studentRepository.save(existingStudent);
+        cacheInvalidationUtil.invalidateCachesAfterCommit(updatedStudent.getId(), CacheConstants.STUDENTS);
 
         return studentMapper.toDto(updatedStudent);
     }
 
-
+    @Transactional
     @Override
     public boolean deleteStudent(Long id) {
         Student student = studentRepository.findById(id).orElse(null);
@@ -71,6 +81,7 @@ public class StudentServiceImpl implements StudentService {
 
         student.setDeleted(true);
         studentRepository.save(student);
+        cacheInvalidationUtil.invalidateCachesAfterCommit(student.getId(), CacheConstants.STUDENTS);
 
         return true;
     }
