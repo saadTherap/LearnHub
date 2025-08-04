e116abd81231621d9631b34f58ede83ed876bf9f
tanvirhassan <tanvir.hassan@therapservices.net>
2025-08-04 09:10:41 +0600

    update-info-topic added for kafka

---
+++ courseConfigurator/src/main/java/net/therap/app/config/KafkaTopicConfig.java
@@ -0,0 +1,22 @@
+package net.therap.app.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+/**
+ * @author tanvirhassan
+ * @since 4/8/25
+ */
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic infoUpdateTopic() {
+        return TopicBuilder.name("update-info-topic")
+                .partitions(10)
+                .replicas(3)
+                .build();
+    }
+}
