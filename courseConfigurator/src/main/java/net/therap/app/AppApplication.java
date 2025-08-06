823a0e5a540ae99342b02836fc52a6a69d79c5d4
Riad Ahmed Anonto <riadanonto@riad.anonto-laptop>
2025-08-06 15:50:23 +0600

    connected service registry

--- courseConfigurator/src/main/java/net/therap/app/AppApplication.java
+++ courseConfigurator/src/main/java/net/therap/app/AppApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
 import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {
 		CacheAutoConfiguration.class,
 		HazelcastAutoConfiguration.class,
 		HazelcastHealthContributorAutoConfiguration.class
 })
+@EnableScheduling
 public class AppApplication {
 
 	public static void main(String[] args) {
