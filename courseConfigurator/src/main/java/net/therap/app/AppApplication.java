a075114326f6e68168d73bb1f87b679926ab0394
gazizafor <gazi.zafor@therapservices.net>
2025-08-07 12:56:38 +0600

    Content reordering inside module implemented

--- courseConfigurator/src/main/java/net/therap/app/AppApplication.java
+++ courseConfigurator/src/main/java/net/therap/app/AppApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {
@@ -15,12 +16,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 		HazelcastAutoConfiguration.class,
 		HazelcastHealthContributorAutoConfiguration.class
 })
+@EnableJpaRepositories(basePackages = "net.therap.app.repository")
 @EnableScheduling
 @EntityScan("net.therap.app.model")
+//@EnableFeignClients(basePackages = "net.therap.auth.client")
+//@ComponentScan(basePackages = "net.therap.auth")
 public class AppApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
-
+	
 }
