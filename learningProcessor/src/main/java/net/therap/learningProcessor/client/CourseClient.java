e206dec643710e93cfab7d075ab33c0c09713451
Apurbo Banik Turjo <apurbo.turjo@therapservices.net>
2025-08-11 12:23:39 +0600

    Log at course client

--- learningProcessor/src/main/java/net/therap/learningProcessor/client/CourseClient.java
+++ learningProcessor/src/main/java/net/therap/learningProcessor/client/CourseClient.java
@@ -3,7 +3,13 @@ package net.therap.learningProcessor.client;
 import net.therap.learningProcessor.dto.*;
 import net.therap.learningProcessor.dto.content.BaseContentDto;
 import net.therap.learningProcessor.dto.content.ContentDetailDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,49 +23,65 @@ import java.util.Map;
  */
 @Component
 public class CourseClient {
-
+    
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private static final String SERVICE_NAME = "course-configurator";
-    private static final String BASE_PATH = "/course-configurator/api";
-
+    private static final String BASE_PATH = "/api/course-configurator";
+    
     @Autowired
     private ServiceDiscoveryCache serviceDiscoveryCache;
-
-    private final RestTemplate restTemplate = new RestTemplate();
-
+    
+    private final RestTemplate restTemplate;
+    
+    @Autowired
+    public CourseClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+    
     private String getServiceBaseUrl() {
+        logger.info("called service base url");
         Map<String, Object> instance = serviceDiscoveryCache.getInstance(SERVICE_NAME);
         String host = (String) instance.get("host");
         Integer port = (Integer) instance.get("port");
+        
+        logger.info("host: {}, port: {}", host, port);
+        
         return "http://" + host + ":" + port + BASE_PATH;
     }
-
-    public List<CourseCatalogDto> getAllCourseCatalogs() {
-        String url = getServiceBaseUrl() + "/courses/public";
-        return restTemplate.getForObject(url, List.class);
-    }
-
+    
     public CourseCatalogDto getCourseCatalog(Long courseId) {
         String url = getServiceBaseUrl() + "/courses/public/" + courseId;
+        
         return restTemplate.getForObject(url, CourseCatalogDto.class);
     }
-
+    
+    public List<CourseCatalogDto> getAllCourseCatalogs() {
+        String url = getServiceBaseUrl() + "/courses/public";
+        
+        return restTemplate.getForObject(url, List.class);
+    }
+    
     public CourseDetailWithProgressDto getCourseDetail(Long courseId) {
         String url = getServiceBaseUrl() + "/courses/" + courseId;
+        
         return restTemplate.getForObject(url, CourseDetailWithProgressDto.class);
     }
-
+    
     public List<ModuleWithProgressDto> getModulesByCourse(Long courseId) {
         String url = getServiceBaseUrl() + "/modules/byCourse/" + courseId;
+        
         return restTemplate.getForObject(url, List.class);
     }
-
+    
     public List<BaseContentDto> getContentsByModule(Long moduleId) {
         String url = getServiceBaseUrl() + "/contents/byModule/" + moduleId;
+        
         return restTemplate.getForObject(url, List.class);
     }
-
+    
     public ContentDetailDto getContentDetail(Long contentId) {
         String url = getServiceBaseUrl() + "/contents/detail/" + contentId;
+        
         return restTemplate.getForObject(url, ContentDetailDto.class);
     }
 }
