a408ebaaf3db30dc2155df7c065e3894b34231c3
Apurbo Banik Turjo <apurbo.turjo@therapservices.net>
2025-08-19 19:30:11 +0600

    Changed the login reponse

---
+++ authserver/src/main/java/net/therap/auth/server/dto/LoginResponse.java
@@ -0,0 +1,16 @@
+package net.therap.auth.server.dto;
+
+import lombok.Data;
+
+/**
+ * @author apurboturjo
+ * @since 8/19/25
+ */
+@Data
+public class LoginResponse {
+    
+    private String email;
+    private String role;
+    private String accessToken;
+    private String refreshToken;
+}
