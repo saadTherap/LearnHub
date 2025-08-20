f2945ea33f272000ca55ee9474fd18bc1d999000
Apurbo Banik Turjo <apurbo.turjo@therapservices.net>
2025-08-20 15:47:12 +0600

    Admin controller endpoints changed; /admin/me added; findAllSorted users (of the platform)

--- authserver/src/main/java/net/therap/auth/server/controller/AuthController.java
+++ authserver/src/main/java/net/therap/auth/server/controller/AuthController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
     
     private final AuthService authService;
