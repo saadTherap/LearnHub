5ec7bd28fc07f6e122abef9a5d594dc902ac2215
Avi Dewan <avi.dewan@therapservices.net>
2025-08-28 09:38:15 +0600

    Auth Filter added in secure file server

--- secureFileServer/src/main/java/net/therap/secureFileServer/SecureFileServerApplication.java
+++ secureFileServer/src/main/java/net/therap/secureFileServer/SecureFileServerApplication.java
@@ -2,8 +2,18 @@ package net.therap.secureFileServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = {"net.therap.auth.lib.client"})
+@EnableScheduling
+@ComponentScan(basePackages = {
+		"net.therap.secureFileServer",
+		"net.therap.auth.lib"
+}
+)
 public class SecureFileServerApplication {
 
 	public static void main(String[] args) {
